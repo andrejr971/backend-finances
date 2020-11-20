44fa7670cc1caf204a6a34d598c3b3459553ea2b
andrejr971 <andre.souza64@fatec.sp.gov.br>
2020-11-19 22:05:34 -0300

    Backend 1.1

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,26 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import Env from '@ioc:Adonis/Core/Env'
+import Youch from 'youch'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error, { response, request }) {
+    if (error.name === 'ValidationException') {
+      return response.status(error.status).send(error.messages)
+    }
+
+    if (Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request)
+      const errorJSON = await youch.toJSON()
+
+      return response.status(error.status).send(errorJSON)
+    }
+
+    return response.status(500).send('Internal Error')
   }
 }
